1dc7038b5839aa052ccad4c0303b8c5a86340955
ycjcl868 <45808948@qq.com>
2019-07-28 18:16:25 +0800

    fix: test ci

---
+++ packages/@umijs/plugin-prerender/test/build.test.js
@@ -0,0 +1,43 @@
+import { join } from 'path';
+import { fork } from 'child_process';
+import { existsSync, readdirSync } from 'fs';
+
+const fixtures = join(__dirname, 'fixtures');
+let dirs = readdirSync(fixtures).filter(dir => dir.charAt(0) !== '.');
+const testOnly = dirs.some(dir => /-only/.test(dir));
+if (testOnly) {
+  dirs = dirs.filter(dir => /-only/.test(dir));
+}
+dirs = dirs.filter(dir => !/^x-/.test(dir));
+
+
+async function umiBuild(cwd) {
+  return new Promise((resolve, reject) => {
+    const umiPath = join(__dirname, '..', 'node_modules', '.bin', 'umi');
+    const env = {
+      COMPRESS: 'none',
+      PROGRESS: 'none',
+      COVERAGE: 1,
+    };
+    const child = fork(umiPath, ['build'], {
+      cwd,
+      env,
+    });
+    child.on('exit', code => {
+      if (code === 1) {
+        reject(new Error('Build failed'));
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+describe('build', () => {
+  require('test-build-result')({
+    root: join(__dirname, './fixtures'),
+    build: async ({ cwd, dir }) => {
+      await umiBuild(cwd);
+    },
+  });
+})
---
+++ packages/@umijs/plugin-prerender/test/fixtures/normal/.umirc.js
@@ -0,0 +1,6 @@
+export default {
+  ssr: true,
+  plugins: [
+    ['../../../lib/index.js']
+  ]
+}
