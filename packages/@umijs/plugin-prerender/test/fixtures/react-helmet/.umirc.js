54163da63002f54e2fecf7377137102a1d9b8e2f
ycjcl868 <45808948@qq.com>
2019-08-13 16:36:25 +0800

    init

--- packages/@umijs/plugin-prerender/test/fixtures/react-helmet/.umirc.js
+++ packages/@umijs/plugin-prerender/test/fixtures/react-helmet/.umirc.js
@@ -1,5 +1,9 @@
+
 export default {
   ssr: true,
+  externals: {
+    'react-helmet': 'react-helmet',
+  },
   plugins: [
     ['../../../lib/index.js', {
       runInMockContext: {
@@ -10,6 +14,8 @@ export default {
       postProcessHtml: ($, path) => {
         const { Helmet } = require('react-helmet');
         Helmet.canUseDOM = false;
+        console.log('HelmetHelmet', Helmet.contextType)
+        console.log('peekpeekpeek', Helmet.peek());
         const helmet = Helmet.renderStatic();
         console.log('aaaa', helmet.title.toString());
         console.log('title origin', $('title').text());
