75dc653167bb47cee7b5119179a3c68c21f5da28
xiaohuoni <448627663@qq.com>
2020-02-12 17:13:24 +0800

    feat: umi-plugin-routes 迁移 umi@3

---
+++ packages/plugin-routes/src/index.ts
@@ -0,0 +1,47 @@
+import { IApi } from 'umi';
+import assert from 'assert';
+import exclude from './exclude';
+
+function optsToArray(item: any) {
+  if (!item) return [];
+  if (Array.isArray(item)) {
+    return item;
+  } else {
+    return [item];
+  }
+}
+
+interface IOpts {
+  exclude: string[] | string;
+  update: Function;
+}
+
+export default function(api: IApi) {
+  // disable if routes if configured
+  if (api.userConfig.routes) return;
+
+  const routesExtend = api.userConfig.routesExtend;
+
+  api.describe({
+    key: 'routesExtend',
+    config: {
+      schema(joi) {
+        return joi.object();
+      },
+    },
+  });
+
+  api.modifyRoutes(routes => {
+    routes = exclude(routes, optsToArray(routesExtend.exclude));
+
+    if (routesExtend.update) {
+      assert(
+        typeof routesExtend.update === 'function',
+        `opts.update should be function, but got ${routesExtend.update}`,
+      );
+      routes = routesExtend.update(routes);
+    }
+
+    return routes;
+  });
+}
