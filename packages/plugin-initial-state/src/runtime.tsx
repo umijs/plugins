427dc329141d3fb367d60efe6849887cc9cba2d6
chencheng <sorrycc@gmail.com>
2020-02-26 19:57:22 +0800

    fix(plugin-initial-state): no path deps in runtime

--- packages/plugin-initial-state/src/runtime.tsx
+++ packages/plugin-initial-state/src/runtime.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { DIR_NAME } from './constants';
 
 export function rootContainer(container: React.ReactNode) {
   return React.createElement(
-    require(`@@/${DIR_NAME}/Provider`).default,
+    // 这里的 plugin-initial-state 不能从 constant 里取，里面有 path 依赖
+    // 但 webpack-5 没有 node 补丁（包括 path）
+    require(`@@/plugin-initial-state/Provider`).default,
     null,
     container,
   );
