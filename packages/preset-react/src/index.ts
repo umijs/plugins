e9436b5ebdb7f550bda3a25e6159674f5bad306d
chencheng <sorrycc@gmail.com>
2020-03-01 21:57:48 +0800

    feat: add plugin-analytics to preset-react

--- packages/preset-react/src/index.ts
+++ packages/preset-react/src/index.ts
@@ -1,15 +1,16 @@
 export default () => {
   return {
     plugins: [
+      require.resolve('@umijs/plugin-access'),
+      require.resolve('@umijs/plugin-analytics'),
       require.resolve('@umijs/plugin-antd'),
+      require.resolve('@umijs/plugin-crossorigin'),
       require.resolve('@umijs/plugin-dva'),
+      require.resolve('@umijs/plugin-initial-state'),
       require.resolve('@umijs/plugin-locale'),
       require.resolve('@umijs/plugin-layout'),
-      require.resolve('@umijs/plugin-access'),
-      require.resolve('@umijs/plugin-initial-state'),
       require.resolve('@umijs/plugin-model'),
       require.resolve('@umijs/plugin-request'),
-      require.resolve('@umijs/plugin-crossorigin'),
     ],
   };
 };
