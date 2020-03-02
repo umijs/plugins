464452b1e04909991d28bf071a3000e3c1cdf7c1
Troy Li <troy.lty@antfin.com>
2020-03-02 13:19:17 +0800

    fix: winPath in directory

--- packages/plugin-initial-state/src/constants.ts
+++ packages/plugin-initial-state/src/constants.ts
@@ -1,8 +1,10 @@
 import { join } from 'path';
+import { utils } from 'umi';
+const { winPath } = utils;
 
 export const DIR_NAME = 'plugin-initial-state';
 export const MODEL_NAME = 'initialState';
-export const RELATIVE_MODEL = join(DIR_NAME, 'models', MODEL_NAME);
+export const RELATIVE_MODEL = winPath(join(DIR_NAME, 'models', MODEL_NAME));
 export const RELATIVE_MODEL_PATH = `${RELATIVE_MODEL}.ts`;
-export const RELATIVE_EXPORT = join(DIR_NAME, 'exports');
+export const RELATIVE_EXPORT = winPath(join(DIR_NAME, 'exports'));
 export const RELATIVE_EXPORT_PATH = `${RELATIVE_EXPORT}.ts`;
