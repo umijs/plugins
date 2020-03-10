b9812ac13a5efbcc16c3f321124da65d5f3474ee
ycjcl868 <45808948@qq.com>
2020-03-10 23:14:20 +0800

    fix: preset react

--- packages/preset-ant-design-pro/src/index.ts
+++ packages/preset-ant-design-pro/src/index.ts
@@ -1,18 +1,13 @@
 import { IApi } from 'umi';
 
 export default (api: IApi) => {
-  const getPlugin = (
-    pkg: string,
-    type: 'preset' | 'plugin' = 'plugin',
-  ): string[] => {
-    const existPlugin = type === 'preset' ? api.hasPresets : api.hasPlugins;
-    if (!existPlugin([pkg])) {
+  const getPlugin = (pkg: string): string[] => {
+    if (!api.hasPlugins([pkg])) {
       return [pkg];
     }
     return [];
   };
   return {
-    presets: [...getPlugin('@umijs/preset-react')],
     plugins: [
       ...getPlugin('umi-plugin-antd-icon-config'),
       ...getPlugin('umi-plugin-antd-theme'),
