dcf21606e7762f5f8d31c1c975be12ffbb87be4e
chencheng <sorrycc@gmail.com>
2020-03-10 09:33:47 +0800

    fix: sass options missing, Close #75

--- packages/plugin-sass/src/index.ts
+++ packages/plugin-sass/src/index.ts
@@ -3,10 +3,13 @@ import { IApi, utils } from 'umi';
 export default (api: IApi) => {
   api.describe({
     config: {
-      schema(joi) {
-        return joi.object({
-          implementation: joi.any(),
-          sassOptions: joi.object(),
+      schema(Joi) {
+        return Joi.object({
+          implementation: Joi.any(),
+          sassOptions: Joi.object(),
+          prependData: Joi.alternatives(Joi.string(), Joi.func()),
+          sourceMap: Joi.boolean(),
+          webpackImporter: Joi.boolean(),
         });
       },
     },
