e90688b257ae9c4c2a1ba10537e53feadc26dd15
ycjcl868 <45808948@qq.com>
2020-03-10 23:05:44 +0800

    feat: @umijs/preset-ant-design-pro

---
+++ packages/preset-ant-design-pro/src/index.ts
@@ -0,0 +1,17 @@
+import { IApi } from 'umi';
+
+export default (api: IApi) => {
+  const getPlugin = (pkg: string): string[] => {
+    if (!api.hasPlugins([pkg])) {
+      return [pkg];
+    }
+    return [];
+  };
+  return {
+    plugins: [
+      ...getPlugin('umi-plugin-antd-icon-config'),
+      ...getPlugin('umi-plugin-antd-theme'),
+      ...getPlugin('umi-plugin-pro-block'),
+    ],
+  };
+};
