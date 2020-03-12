66f3de266d6ae0f22964b159e58f256ebdb4bdf4
信鑫-King <chaolin.jcl@alibaba-inc.com>
2020-03-12 17:54:46 +0800

    feat: @umijs/preset-ant-design-pro (#81)
    * feat: @umijs/preset-ant-design-pro
    
    * fix: pro preset
    
    * fix: add @umijs/preset-react
    
    * fix: preset react
    
    * fix: package.json
    
    * fix: block devtool
    
    * fix: update icon version
    
    * fix: remove theme plugin
    
    * chore: include umi-plugin-antd-icon-config if plugin-react is not valid
    
    * fix: typo
    
    Co-authored-by: chencheng <sorrycc@gmail.com>

---
+++ packages/preset-ant-design-pro/src/index.ts
@@ -0,0 +1,13 @@
+import { IApi } from 'umi';
+
+export default (api: IApi) => {
+  return {
+    plugins: [
+      // 有 plugin-react 时不加 plugin-antd-icon-config，因为已经内置了
+      // 在 ant-design-pro 5 之后，用 plugin-react，去 plugin-antd-icon-config
+      !api.hasPlugins(['@umijs/plugin-react']) &&
+        require.resolve('umi-plugin-antd-icon-config'),
+      require.resolve('@umijs/plugin-block-devtool'),
+    ].forEach(Boolean),
+  };
+};
