5da125b5466fde93cfb5090a39aa0f3736f2dde0
chencheng <sorrycc@gmail.com>
2020-03-12 17:25:28 +0800

    chore: include umi-plugin-antd-icon-config if plugin-react is not valid

--- packages/preset-ant-design-pro/src/index.ts
+++ packages/preset-ant-design-pro/src/index.ts
@@ -1,16 +1,13 @@
 import { IApi } from 'umi';
 
 export default (api: IApi) => {
-  const getPlugin = (pkg: string): string[] => {
-    if (!api.hasPlugins([pkg])) {
-      return [pkg];
-    }
-    return [];
-  };
   return {
     plugins: [
-      ...getPlugin('umi-plugin-antd-icon-config'),
-      ...getPlugin('@umijs/plugin-block-devtool'),
-    ],
+      // 有 plugin-react 时不加 plugin-antd-icon-config，因为已经内置了
+      // 在 ant-design-pro 5 之后，用 plugin-react，去 plugin-antd-icon-config
+      api.hasPlugins(['@umijs/plugin-react']) &&
+        require.resolve('umi-plugin-antd-icon-config'),
+      require.resolve('@umijs/plugin-block-devtool'),
+    ].forEach(Boolean),
   };
 };
