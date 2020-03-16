3e7fb00ae7a6d911c795def29623856bbb970b3d
imhele <work@imhele.com>
2020-03-16 21:39:54 +0800

    fix(dva-types): filter sub object

--- packages/plugin-dva/src/getModelExports.ts
+++ packages/plugin-dva/src/getModelExports.ts
@@ -4,51 +4,61 @@ import ts from 'typescript';
 import { tsquery } from '@phenomnomnominal/tsquery';
 
 export function getModelExports(path: string) {
+  const defaultNamespace = basename(path, extname(path));
+
   const code = readFileSync(path, 'utf8');
   const source = ts.createSourceFile('', code, ts.ScriptTarget.Latest, true);
-  const exportNode = getExportDefaultNode(source);
-  const effectsNodes = tsquery(
-    exportNode,
-    `PropertyAssignment:has(Identifier[name=effects], StringLiteral[value=effects])`,
-  ) as ts.PropertyAssignment[];
-  const reducersNodes = tsquery(
-    exportNode,
-    `PropertyAssignment:has(Identifier[name=reducers], StringLiteral[value=reducers])`,
-  ) as ts.PropertyAssignment[];
-  const namespaceNodes = tsquery(
-    exportNode,
-    `PropertyAssignment:has(Identifier[name=namespace], StringLiteral[value=namespace]) \
-      > StringLiteral`,
-  ) as ts.StringLiteral[];
+  const exportObject = getExportDefaultNode(source);
+  if (!exportObject) return { path, namespace: defaultNamespace };
 
-  const effectsNode = effectsNodes[effectsNodes.length - 1] as
-    | ts.PropertyAssignment
-    | undefined;
-  const reducersNode = reducersNodes[reducersNodes.length - 1] as
-    | ts.PropertyAssignment
-    | undefined;
-  const namespaceNode = namespaceNodes[namespaceNodes.length - 1] as
-    | ts.StringLiteral
-    | undefined;
+  const effectsNodes = exportObject.properties.filter(
+    (prop): prop is ts.PropertyAssignment =>
+      prop.kind === ts.SyntaxKind.PropertyAssignment &&
+      prop.name?.getText() === 'effects',
+  );
+  const reducersNodes = exportObject.properties.filter(
+    (prop): prop is ts.PropertyAssignment =>
+      prop.kind === ts.SyntaxKind.PropertyAssignment &&
+      prop.name?.getText() === 'reducers',
+  );
+  const namespaceNodes = exportObject.properties.filter(
+    (prop): prop is ts.PropertyAssignment =>
+      prop.kind === ts.SyntaxKind.PropertyAssignment &&
+      prop.name?.getText() === 'namespace',
+  );
+
+  const effectsNode = effectsNodes[effectsNodes.length - 1];
+  const reducersNode = reducersNodes[reducersNodes.length - 1];
+  const namespaceNode = namespaceNodes[namespaceNodes.length - 1];
+
+  const namespaceValue = isStringLiteral(namespaceNode?.initializer)
+    ? namespaceNode.initializer.text
+    : null;
 
   return {
     path,
     effects: effectsNode && getPropertyKeysOfPropertyAssignment(effectsNode),
     reducers: reducersNode && getPropertyKeysOfPropertyAssignment(reducersNode),
-    namespace: namespaceNode?.text ?? basename(path, extname(path)),
+    namespace: namespaceValue ?? defaultNamespace,
   };
 }
 
-function getExportDefaultNode(node: ts.SourceFile) {
+function isStringLiteral(node: ts.Node | undefined): node is ts.StringLiteral {
+  return !!node && node.kind === ts.SyntaxKind.StringLiteral;
+}
+
+function getExportDefaultNode(
+  node: ts.SourceFile,
+): ts.ObjectLiteralExpression | undefined {
   const exportAssignments = tsquery(node, `SourceFile > ExportAssignment`);
   const exportAssignment = exportAssignments[exportAssignments.length - 1];
 
-  if (!exportAssignment) return node;
+  if (!exportAssignment) return;
 
   const exportObjects = tsquery(
     exportAssignment,
     `ExportAssignment > ObjectLiteralExpression`,
-  );
+  ) as ts.ObjectLiteralExpression[];
 
   if (exportObjects.length) return exportObjects[exportObjects.length - 1];
 
@@ -66,13 +76,13 @@ function getExportDefaultNode(node: ts.SourceFile) {
           > VariableDeclarationList \
             > VariableDeclaration:has(Identifier[name="${identifier.getText()}"]) \
               > ObjectLiteralExpression`,
-    );
+    ) as ts.ObjectLiteralExpression[];
 
     if (variableObjects.length)
       return variableObjects[variableObjects.length - 1];
   }
 
-  return node;
+  return;
 }
 
 function getPropertyKeysOfPropertyAssignment(node: ts.PropertyAssignment) {
