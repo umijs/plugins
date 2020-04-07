aa21ad4dec46c377eac8cb8d566d8b0342e2f889
chencheng <sorrycc@gmail.com>
2020-04-07 14:36:07 +0800

    feat: add plugin-preact

---
+++ packages/plugin-preact/src/index.ts
@@ -0,0 +1,28 @@
+import { IApi } from 'umi';
+import { dirname, join } from 'path';
+
+export default (api: IApi) => {
+  api.chainWebpack(memo => {
+    memo.resolve.alias
+      .set('preact/devtools', require.resolve('preact/devtools'))
+      .set('preact/hooks', require.resolve('preact/hooks'))
+      .set('preact', require.resolve('preact'))
+      .set('react', dirname(require.resolve('preact/compat/package.json')))
+      .set('react-dom', dirname(require.resolve('preact/compat/package.json')))
+      .set(
+        'create-react-class',
+        require.resolve('preact-compat/lib/create-react-class'),
+      );
+    return memo;
+  });
+
+  api.addEntryImports(() => {
+    return process.env.NODE_ENV === 'development'
+      ? [
+          {
+            source: 'preact/devtools',
+          },
+        ]
+      : [];
+  });
+};
