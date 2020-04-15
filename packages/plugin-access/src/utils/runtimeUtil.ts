6e4bf048991468faf44fc4f39d588fe677bf1694
Suyi <thonatos@users.noreply.github.com>
2020-04-15 16:47:48 +0800

    🐛 bug: render menu where initialState changed (#162)
    * fix(access): should return immer routes
    
    * fix(layout): should render when initialState changed
    
    * fix(access): handle no child routes

--- packages/plugin-access/src/utils/runtimeUtil.ts
+++ packages/plugin-access/src/utils/runtimeUtil.ts
@@ -4,7 +4,16 @@ import { IRoute } from 'umi';
 type Routes = IRoute[];
 
 export function traverseModifyRoutes(routes: Routes, access: any) {
-  const resultRoutes: Routes = [].concat(routes as any);
+  const resultRoutes: Routes = []
+    .concat(routes as any)
+    .map((resultRoute: IRoute) => {
+      const { routes } = resultRoute;
+      return {
+        ...resultRoute,
+        // return new route to routes.
+        routes: routes ? routes.map((route: any) => ({ ...route })) : routes,
+      };
+    });
   const notHandledRoutes: Routes = [];
 
   notHandledRoutes.push(...resultRoutes);
