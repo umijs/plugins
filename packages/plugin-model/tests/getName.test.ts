49808646b6991ce13cdced37102bb4b61ce378e7
Qhan W <whenhan@foxmail.com>
2020-04-29 16:05:37 +0800

    fix: File path under Win (#184)
    * fix: 修复 windows 下文件路径问题
    
    * fix: 测试示例路径调整及替换分隔符正则

--- packages/plugin-model/tests/getName.test.ts
+++ packages/plugin-model/tests/getName.test.ts
@@ -1,27 +1,32 @@
 import { getName } from '../src/utils';
+import { join } from 'path';
+
+const srcDir = ['models', 'model', 'pages', 'page'].map(dir =>
+  join('ae/src', dir),
+);
 
 describe('getName test', () => {
   test('global model', () => {
-    expect(getName('ae/src/models/global.ts')).toEqual('global');
+    expect(getName(join('ae/src/models/global.ts'), srcDir)).toEqual('global');
   });
   test('page multiple model', () => {
-    expect(getName('ae/src/pages/dashboard/Workplace/models/list.ts')).toEqual(
-      'dashboard.Workplace.list',
-    );
+    expect(
+      getName(join('ae/src/pages/dashboard/Workplace/models/list.ts'), srcDir),
+    ).toEqual('dashboard.Workplace.list');
   });
   test('page single model', () => {
-    expect(getName('ae/src/pages/dashboard/Workplace/notice.model.ts')).toEqual(
-      'dashboard.Workplace.notice',
-    );
+    expect(
+      getName(join('ae/src/pages/dashboard/Workplace/notice.model.ts'), srcDir),
+    ).toEqual('dashboard.Workplace.notice');
   });
   test('page single model when singular is "page"', () => {
-    expect(getName('ae/src/page/dashboard/Workplace/form.model.ts')).toEqual(
-      'dashboard.Workplace.form',
-    );
+    expect(
+      getName(join('ae/src/page/dashboard/Workplace/form.model.ts'), srcDir),
+    ).toEqual('dashboard.Workplace.form');
   });
   test('other model', () => {
     expect(
-      getName('ae/src/.umi/plugin-initial-state/models/initialState.ts'),
+      getName(join('ae/src/.umi/plugin-initial-state/models/initialState.ts')),
     ).toEqual('initialState');
   });
 });
