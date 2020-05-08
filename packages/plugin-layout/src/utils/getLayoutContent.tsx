2c166b35498d9ceb975cf7dd42ef0343b432e8ad
chenshuai2144 <qixian.cs@outlook.com>
2020-05-08 17:19:28 +0800

    fix test

--- packages/plugin-layout/src/utils/getLayoutContent.tsx
+++ packages/plugin-layout/src/utils/getLayoutContent.tsx
@@ -40,5 +40,4 @@ export default props => {
     ...props
   });
 };
-
 `;
--- packages/plugin-layout/test/getLayoutContent.test.js
+++ packages/plugin-layout/test/getLayoutContent.test.js
@@ -5,25 +5,43 @@ describe('getLayoutContent', () => {
     const LayoutContent = getLayoutContent({}, 'test');
 
     expect(LayoutContent).toEqual(
-      `import React from 'react';
-import { ApplyPluginsType } from 'umi';
-import { plugin } from '../core/umiExports';
-
-export default (props) => {
-  const runtimeConfig = plugin.applyPlugins({
-    key: 'layout',
-    type: ApplyPluginsType.modify,
-    initialValue: {},
-  }) || {};
-  const userConfig = {
-    ...{},
-    ...runtimeConfig
-  };
-  return React.createElement(require('test').default, {
-    userConfig,
-    ...props
-  })
-}
+      `import React, { useState, useEffect } from "react";
+      import { ApplyPluginsType, useModel } from "umi";
+      import { plugin } from "../core/umiExports";
+      
+      export default props => {
+        const [runtimeConfig, setRuntimeConfig] = useState({});
+      
+        const initialInfo = (useModel && useModel("@@initialState")) || {
+          initialState: undefined,
+          loading: false,
+          setInitialState: null
+        }; // plugin-initial-state 未开启
+      
+        useEffect(() => {
+          const useRuntimeConfig =
+            plugin.applyPlugins({
+              key: "layout",
+              type: ApplyPluginsType.modify,
+              initialValue: initialInfo
+            }) || {};
+          if (useRuntimeConfig instanceof Promise) {
+            useRuntimeConfig.then(config => {
+              setRuntimeConfig(config);
+            });
+            return;
+          }
+          setRuntimeConfig(useRuntimeConfig);
+        }, [initialInfo?.initialState]);
+        const userConfig = {
+          ...{},
+          ...runtimeConfig
+        };
+        return React.createElement(require("test").default, {
+          userConfig,
+          ...props
+        });
+      };}
 `,
     );
   });
