6d422d50baaa5444f371a0040566cc3858976e76
chenshuai2144 <qixian.cs@outlook.com>
2020-05-15 10:46:19 +0800

    feat(plugin-layout): more console info

--- packages/plugin-layout/src/layout/blankLayout.tsx
+++ packages/plugin-layout/src/layout/blankLayout.tsx
@@ -1,1 +1,7 @@
-export default ({ children }: { children: any }) => children;
+export default ({ children }: { children: any }) => {
+  console.error(
+    '@umijs/plugin-layout 需要安装 @ant-design/pro-layout 才可运行',
+  );
+  console.error('https://prolayout.ant.design/');
+  return children;
+};
