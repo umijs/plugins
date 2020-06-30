33076e70691d09ab6001f1c7e9ef50672d31ec2f
troy.lty <troy.lty@alipay.com>
2020-06-30 12:02:49 +0800

    feat: dev 环境下的增加数据更新记录

--- packages/plugin-model/src/helpers/executor.tsx
+++ packages/plugin-model/src/helpers/executor.tsx
@@ -16,6 +16,32 @@ export default (props: ExecutorProps) => {
   let data: any;
   try {
     data = hook();
+    if (process.env.NODE_ENV === 'development') {
+      let count = Object.keys(
+        ((window as any)._umi_useModel_dev_tool_log || {})[namespace] || {},
+      ).length;
+      (window as any)._umi_useModel_dev_tool = {
+        ...(window as any)._umi_useModel_dev_tool,
+        [namespace]: data,
+      };
+      (window as any)._umi_useModel_dev_tool_log = {
+        ...(window as any)._umi_useModel_dev_tool_log,
+        [namespace]: {
+          ...((window as any)._umi_useModel_dev_tool_log || {})[namespace],
+          [count]: data,
+        },
+      };
+      window.dispatchEvent(
+        new CustomEvent('_umi_useModel_update', {
+          detail: {
+            namespace,
+            time: Date.now(),
+            data,
+            index: count,
+          },
+        }),
+      );
+    }
   } catch (e) {
     console.error(
       `plugin-model: Invoking '${namespace || 'unknown'}' model failed:`,
