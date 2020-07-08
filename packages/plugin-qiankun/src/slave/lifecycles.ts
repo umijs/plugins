18c15f2f041b7abffa826f665d6f88d9289479c8
信鑫-King <chaolinjin@gmail.com>
2020-07-08 11:06:55 +0800

    feat(qiankun): microApp auto setLoading (#293)

--- packages/plugin-qiankun/src/slave/lifecycles.ts
+++ packages/plugin-qiankun/src/slave/lifecycles.ts
@@ -71,7 +71,20 @@ export function genMount() {
     const historyOptions = normalizeHistory(props?.history, props?.base);
     setCreateHistoryOptions(historyOptions);
 
-    defer.resolve();
+    // 默认修改 loading
+    // 如果需要手动控制 loading
+    // 通过配置 app.ts，slave: { autoSetLanding: false }
+    const callback = () => {
+      if (
+        slaveRuntime?.autoSetLanding !== false &&
+        typeof props?.setLoading === 'function'
+      ) {
+        props.setLoading(false);
+      }
+    };
+    defer.resolve({
+      callback,
+    });
     // 第一次 mount umi 会自动触发 render，非第一次 mount 则需手动触发
     if (hasMountedAtLeastOnce) {
       render();
