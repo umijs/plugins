6450dcca5ded94c94620737e6e644ebe79b351ab
chencheng <sorrycc@gmail.com>
2020-08-24 15:12:03 +0800

    fix: test

--- packages/plugin-layout/test/getLayoutContent.test.js
+++ packages/plugin-layout/test/getLayoutContent.test.js
@@ -4,7 +4,9 @@ describe('getLayoutContent', () => {
   test('getLayoutContent', () => {
     const LayoutContent = getLayoutContent({}, 'test');
 
-    expect(LayoutContent.includes(`require("test")`)).toBeTruthy();
+    expect(
+      LayoutContent.includes(`import LayoutComponent from 'test';`),
+    ).toBeTruthy();
     expect(LayoutContent.includes(`...{}`)).toBeTruthy();
   });
 });
