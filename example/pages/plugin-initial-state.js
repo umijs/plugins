c8078ac31ef9df6d501aa3074f116dfe569afb26
Troy Li <troy.lty@alipay.com>
2020-09-21 11:26:28 +0800

    fix: update state failed when unmount component immediate after update (#381)

--- example/pages/plugin-initial-state.js
+++ example/pages/plugin-initial-state.js
@@ -3,11 +3,13 @@ import { useModel } from 'umi';
 
 export default () => {
   const { initialState, loading, setInitialState } = useModel('@@initialState');
+  const { description } = useModel('bar');
   return (
     <div>
       <h1>
         initial-state: {loading ? 'loading...' : JSON.stringify(initialState)}
       </h1>
+      <h1>count from another model: {description}</h1>
       <button onClick={() => setInitialState({ name: 'a name' })}>
         change name
       </button>
