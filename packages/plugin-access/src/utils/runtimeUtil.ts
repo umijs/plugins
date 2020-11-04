31d87bb8672f1902ad6f6b3dce27bd6450edae9c
chenshuai2144 <qixian.cs@outlook.com>
2020-11-04 17:21:35 +0800

    fix(access): 修复因为可变数据导致的数据不同步的问题

--- packages/plugin-access/src/utils/runtimeUtil.ts
+++ packages/plugin-access/src/utils/runtimeUtil.ts
@@ -3,7 +3,7 @@ import { IRoute } from 'umi';
 
 type Routes = IRoute[];
 
-export function traverseModifyRoutes(routes: Routes, access: any) {
+export function traverseModifyRoutes(routes: Routes, access: any): Routes {
   const resultRoutes: Routes = []
     .concat(routes as any)
     .map((resultRoute: IRoute) => {
@@ -14,16 +14,14 @@ export function traverseModifyRoutes(routes: Routes, access: any) {
         routes: routes ? routes.map((route: any) => ({ ...route })) : routes,
       };
     });
-  const notHandledRoutes: Routes = [];
 
-  notHandledRoutes.push(...resultRoutes);
-
-  for (let i = 0; i < notHandledRoutes.length; i++) {
-    const currentRoute = notHandledRoutes[i];
+  return resultRoutes.map(currentRoute => {
     let currentRouteAccessible =
       typeof currentRoute.unaccessible === 'boolean'
         ? !currentRoute.unaccessible
         : true;
+
+    // 判断路由是否有权限的具体代码
     if (currentRoute && currentRoute.access) {
       if (typeof currentRoute.access !== 'string') {
         throw new Error(
@@ -33,38 +31,45 @@ export function traverseModifyRoutes(routes: Routes, access: any) {
         );
       }
       const accessProp = access[currentRoute.access];
+      // 如果是方法需要执行以下
       if (typeof accessProp === 'function') {
         currentRouteAccessible = accessProp(currentRoute);
       } else if (typeof accessProp === 'boolean') {
+        // 不是方法就直接 copy
         currentRouteAccessible = accessProp;
       }
       currentRoute.unaccessible = !currentRouteAccessible;
     }
 
+    // 筛选子路由
     if (currentRoute.routes || currentRoute.childRoutes) {
       const childRoutes: Routes =
         currentRoute.routes || currentRoute.childRoutes;
+
       if (!Array.isArray(childRoutes)) {
-        continue;
+        return currentRoute;
       }
+      // 父亲没权限，理论上每个孩子都没权限
+      // 可能有打平 的事情发生，所以都执行一下
       childRoutes.forEach(childRoute => {
         childRoute.unaccessible = !currentRouteAccessible;
-      }); // Default inherit from parent route
-      notHandledRoutes.push(...childRoutes);
-    }
-  }
+      });
 
-  // Make parent route unaccessible if child routes exist and all of child routes are unaccessible
-  for (let i = 0; i < notHandledRoutes.length; i++) {
-    const currentRoute = notHandledRoutes[i];
-    const childRoutes: Routes = currentRoute.routes || currentRoute.childRoutes;
-    const isAllChildRoutesUnaccessible =
-      Array.isArray(childRoutes) &&
-      childRoutes.every(route => route.unaccessible);
-    if (!currentRoute.unaccessible && isAllChildRoutesUnaccessible) {
-      currentRoute.unaccessible = true;
-    }
-  }
+      // 如果每个子节点都没有权限，那么自己也属于没有权限
+      const isAllChildRoutesUnaccessible =
+        Array.isArray(childRoutes) &&
+        childRoutes.every(route => route.unaccessible);
 
-  return resultRoutes;
+      if (!currentRoute.unaccessible && isAllChildRoutesUnaccessible) {
+        currentRoute.unaccessible = true;
+      }
+
+      return {
+        ...currentRoute,
+        routes: traverseModifyRoutes(childRoutes, access),
+      };
+    }
+
+    return currentRoute;
+  });
 }
