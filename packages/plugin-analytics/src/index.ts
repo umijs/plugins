ea70c9cac673febeeb7cc24ef831b87d81aad107
dingzhen <dingzhen0720@qq.com>
2020-11-20 18:56:20 +0800

    将谷歌统计追踪代码由analytics.js 升级为 gtag.js

--- packages/plugin-analytics/src/index.ts
+++ packages/plugin-analytics/src/index.ts
@@ -27,23 +27,27 @@ export default (api: IApi) => {
   `;
   };
 
+  // 引入gtag.js
+  const requireGtagJs=(code:string)=>{
+    return `
+    (function() {
+      var gtag = document.createElement("script");
+      gtag.async = true;
+      gtag.src = "https://www.googletagmanager.com/gtag/js?id=${code}";
+      var s = document.getElementsByTagName("script")[0];
+      s.parentNode.insertBefore(gtag, s);
+    })();
+  `;
+  }
+
   const gaTpl = (code: string) => {
     return `
     (function(){
       if (!location.port) {
-        (function (i, s, o, g, r, a, m) {
-          i['GoogleAnalyticsObject'] = r;
-          i[r] = i[r] || function () {
-              (i[r].q = i[r].q || []).push(arguments)
-            }, i[r].l = 1 * new Date();
-          a = s.createElement(o),
-            m = s.getElementsByTagName(o)[0];
-          a.async = 1;
-          a.src = g;
-          m.parentNode.insertBefore(a, m)
-        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-        ga('create', '${code}', 'auto');
-        ga('send', 'pageview');
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${code}');
       }
     })();
   `;
@@ -53,6 +57,14 @@ export default (api: IApi) => {
     api.addHTMLHeadScripts(() => [
       {
         content: 'var _hmt = _hmt || [];',
+      },
+    ]);
+  }
+
+  if (ga) {
+    api.addHTMLHeadScripts(() => [
+      {
+        content: requireGtagJs(ga),
       },
     ]);
   }
