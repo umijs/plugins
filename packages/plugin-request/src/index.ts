ebaf13590493fce04e128834319895862382664d
ycjcl868 <45808948@qq.com>
2020-11-30 23:23:13 +0800

    fix: request include antd

--- packages/plugin-request/src/index.ts
+++ packages/plugin-request/src/index.ts
@@ -1,14 +1,14 @@
-import { IApi } from 'umi';
+import { IApi, utils } from 'umi';
 import { join, dirname } from 'path';
 import { readFileSync } from 'fs';
 
+const { Mustache } = utils;
 export interface RequestOptions {
   dataField?: string;
 }
 
 export default function(api: IApi) {
   const {
-    paths,
     utils: { winPath },
   } = api;
 
@@ -54,12 +54,16 @@ export default function(api: IApi) {
     },
   });
 
-  const source = join(__dirname, '..', 'src', 'request.ts');
-  const requestTemplate = readFileSync(source, 'utf-8');
   const namespace = 'plugin-request';
 
   api.onGenerateFiles(() => {
     const { dataField = 'data' } = api.config.request as RequestOptions;
+    const source = join(__dirname, '..', 'src', 'request.ts');
+    console.log('api.config.antd', api.config.antd);
+    const requestTemplate = Mustache.render(readFileSync(source, 'utf-8'), {
+      // 与 antd 解耦
+      Antd: !!api.config.antd,
+    });
     try {
       // Write .umi/plugin-request/request.ts
       let formatResultStr;
