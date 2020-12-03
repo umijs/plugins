9ed35f69034a6ec7871d334b0bb5245bec294b3d
ycjcl868 <45808948@qq.com>
2020-12-03 22:50:20 +0800

    chore: test case

--- packages/plugin-request/tests/normal.test.js
+++ packages/plugin-request/tests/normal.test.js
@@ -5,7 +5,7 @@ import createTestServer from './createTestServer';
 import { request } from '../src/request';
 
 jest.mock('umi', () => require('./mocks/umi'));
-jest.mock('antd', () => require('./mocks/antd'));
+jest.mock('@umijs/plugin-request/lib/ui', () => require('./mocks/antd'));
 
 jest.mock(
   'runtimeConfig',
