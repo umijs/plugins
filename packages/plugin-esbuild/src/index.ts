b7c17c1c44b1403898c3a4c05e89f4216d20ec99
信鑫-King <chaolinjin@gmail.com>
2020-12-15 23:21:29 +0800

    chore: using esbuild-loader (#478)

--- packages/plugin-esbuild/src/index.ts
+++ packages/plugin-esbuild/src/index.ts
@@ -1,4 +1,5 @@
 import { IApi, BundlerConfigType } from 'umi';
+import { ESBuildPlugin, ESBuildMinifyPlugin } from 'esbuild-loader';
 
 export default (api: IApi) => {
   api.describe({
@@ -15,16 +16,20 @@ export default (api: IApi) => {
     if (memo.optimization) {
       const optsMap = {
         [BundlerConfigType.csr]: {
-          target: 'chrome49',
+          target: 'es2015',
+          minify: true,
         },
         [BundlerConfigType.ssr]: {
           target: 'node10',
+          minify: true,
         },
       };
       const opts = optsMap[type] || optsMap[BundlerConfigType.csr];
-      memo.optimization.minimizer = [
-        new (require('esbuild-webpack-plugin').default)(opts),
-      ];
+      memo.optimization.minimize = true;
+      memo.optimization.minimizer = [new ESBuildMinifyPlugin(opts)];
+    }
+    if (memo.plugins) {
+      memo.plugins.push(new ESBuildPlugin());
     }
     return memo;
   });
