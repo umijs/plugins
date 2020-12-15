ddf612d2a38dec1aa1b2b75c55f440d7b06d6fd8
ycjcl868 <45808948@qq.com>
2020-12-15 18:11:29 +0800

    chore: speed up

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  transformIgnorePatterns: [
+    'node_modules/(?!.*(@(babel)|dva-loading))[^/]+?/(?!(es|node_modules)/)',
+  ],
   moduleNameMapper(memo) {
     return Object.assign(memo, {
       '^react$': require.resolve('react'),
