934045aee7cb80f1708af998fb8d1d7ccbb01c89
ycjcl868 <45808948@qq.com>
2020-12-15 17:08:18 +0800

    chore: test case

--- packages/plugin-esbuild/src/index.test.ts
+++ packages/plugin-esbuild/src/index.test.ts
@@ -3,42 +3,54 @@ import { Service } from 'umi';
 
 const fixtures = join(__dirname, 'fixtures');
 
-test('normal build', async () => {
-  const cwd = join(fixtures, 'normal');
-  const service = new Service({
-    cwd,
-    env: 'production',
-    plugins: [require.resolve('./index.ts')],
-  });
-  let err;
-  try {
-    await service.run({
-      name: 'build',
+describe('normal build', () => {
+  let err: any;
+  beforeAll(async () => {
+    const cwd = join(fixtures, 'normal');
+    const service = new Service({
+      cwd,
+      env: 'production',
+      plugins: [require.resolve('./index.ts')],
     });
-    err = false;
-  } catch (e) {
-    console.error('normal build error', e);
-    err = true;
-  }
-  expect(err).toBeFalsy();
+    let err;
+    try {
+      await service.run({
+        name: 'build',
+      });
+      err = false;
+    } catch (e) {
+      console.error('normal build error', e);
+      err = true;
+    }
+  });
+
+  it('normal', () => {
+    expect(err).toBeFalsy();
+  });
 });
 
-test('ssr build', async () => {
-  const cwd = join(fixtures, 'ssr');
-  const service = new Service({
-    cwd,
-    env: 'production',
-    plugins: [require.resolve('./index.ts')],
-  });
-  let err;
-  try {
-    await service.run({
-      name: 'build',
+describe('ssr build', async () => {
+  let err: any;
+  beforeAll(async () => {
+    const cwd = join(fixtures, 'ssr');
+    const service = new Service({
+      cwd,
+      env: 'production',
+      plugins: [require.resolve('./index.ts')],
     });
-    err = false;
-  } catch (e) {
-    console.error('ssr build error', e);
-    err = true;
-  }
-  expect(err).toBeFalsy();
+    let err;
+    try {
+      await service.run({
+        name: 'build',
+      });
+      err = false;
+    } catch (e) {
+      console.error('ssr build error', e);
+      err = true;
+    }
+  });
+
+  it('ssr build', () => {
+    expect(err).toBeFalsy();
+  });
 });
