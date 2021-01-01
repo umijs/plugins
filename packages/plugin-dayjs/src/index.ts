61b75c61d94ac2e67e815e0243b4d7358a2840d6
iamkun <kunhello@outlook.com>
2021-01-01 17:00:12 +0800

    feat: add umi-dayjs-plugin

---
+++ packages/plugin-dayjs/src/index.ts
@@ -0,0 +1,23 @@
+import { IApi } from 'umi';
+import AntdDayjsWebpackPlugin from 'antd-dayjs-webpack-plugin';
+
+export default (api: IApi) => {
+  api.describe({
+    key: 'dayjs',
+    config: {
+      schema(joi) {
+        return joi.string();
+      },
+    },
+  });
+
+  const { dayjs } = api.userConfig;
+
+  api.chainWebpack((config) => {
+    config
+      .plugin('antd-dayjs-webpack-plugin')
+      .use(AntdDayjsWebpackPlugin, [dayjs])
+      .end();
+    return config;
+  });
+};
