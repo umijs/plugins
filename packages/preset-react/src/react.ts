5d9ba02aab752a12c7505eb60f65843f3d6df84c
Yingzhi Ji <jiyingzhi0808@gmail.com>
2021-01-03 21:52:59 -0600

    fix: remove babel-plugin-react-require when using automatic runtime (#497)
    * fix: remove babel-plugin-react-require when use automatic runtime
    
    * fix: remove babel-plugin-react-require when using automatic runtime in React17

--- packages/preset-react/src/react.ts
+++ packages/preset-react/src/react.ts
@@ -36,6 +36,7 @@ export default (api: IApi) => {
   api.modifyBabelPresetOpts(opts => {
     return {
       ...opts,
+      reactRequire: !isReact17(),
       react: {
         ...(opts.react || {}),
         // support React 17 New Jsx syntax
