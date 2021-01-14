f37d07b53b50ec57c8300964aff1bcfb0d563284
Kuitos <kuitos.lau@gmail.com>
2021-01-14 20:42:46 +0800

    feat(qiankun): 移除子应用对 mountElementId 的变更 (#481)

--- packages/plugin-qiankun/src/constants.ts
+++ packages/plugin-qiankun/src/constants.ts
@@ -1,5 +1,4 @@
 export const defaultMasterRootId = 'root-master';
-export const defaultSlaveRootId = 'root-slave';
 export const defaultHistoryType = 'browser';
 export const qiankunStateForSlaveModelNamespace = '@@qiankunStateForSlave';
 export const qiankunStateFromMasterModelNamespace = '@@qiankunStateFromMaster';
