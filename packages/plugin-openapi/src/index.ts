dfffd2e2b078d85495a4b439347d2bc95343304d
chenshuai2144 <qixian.cs@outlook.com>
2021-01-24 20:09:16 +0800

    fix bug

--- packages/plugin-openapi/src/index.ts
+++ packages/plugin-openapi/src/index.ts
@@ -5,7 +5,7 @@ import { existsSync, mkdirSync } from 'fs';
 
 export default (api: IApi) => {
   api.describe({
-    key: 'openapi',
+    key: 'openApi',
     config: {
       schema(joi) {
         return joi.object();
@@ -15,28 +15,18 @@ export default (api: IApi) => {
   });
 
   api.registerCommand({
-    name: 'oneapi',
+    name: 'openapi',
     fn: async () => {
-      const openAPIConfig = await api.applyPlugins({
-        key: 'oneapi',
-        type: api.ApplyPluginsType.add,
-        initialValue: {
-          mock: false,
-        },
-      });
-
+      const openAPIConfig = api.config.openapi;
       const pageConfig = require(join(api.cwd, 'package.json'));
       const mockFolder = openAPIConfig.mock
         ? join(api.cwd, 'mocks')
         : undefined;
-
       const serversFolder = join(api.cwd, 'src', 'servers');
-
       // 如果mock 文件不存在，创建一下
       if (mockFolder && !existsSync(mockFolder)) {
         mkdirSync(mockFolder);
       }
-
       // 如果mock 文件不存在，创建一下
       if (serversFolder && !existsSync(serversFolder)) {
         mkdirSync(serversFolder);
