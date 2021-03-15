35a299a407dc766f93d5a1d81120622ff13a16af
chenshuai2144 <qixian.cs@outlook.com>
2021-03-15 18:39:34 +0800

    feat(openapi): support openAPIConfig is array

--- packages/plugin-openapi/src/index.ts
+++ packages/plugin-openapi/src/index.ts
@@ -10,12 +10,22 @@ export default (api: IApi) => {
     key: 'openAPI',
     config: {
       schema(joi) {
-        return joi.object({
-          requestLibPath: joi.string(),
-          schemaPath: joi.string(),
-          mock: joi.boolean(),
-          projectName: joi.string(),
-        });
+        return joi.alternatives(
+          joi.array().items(
+            joi.object({
+              requestLibPath: joi.string(),
+              schemaPath: joi.string(),
+              mock: joi.boolean(),
+              projectName: joi.string(),
+            }),
+          ),
+          joi.object({
+            requestLibPath: joi.string(),
+            schemaPath: joi.string(),
+            mock: joi.boolean(),
+            projectName: joi.string(),
+          }),
+        );
       },
     },
     enableBy: api.EnableBy.config,
@@ -38,21 +48,43 @@ export default (api: IApi) => {
   });
 
   api.onGenerateFiles(() => {
+    const openAPIConfig = api.config.openAPI;
     api.writeTmpFile({
       path: join('plugin-openapi', 'openapi.tsx'),
       content: `
+import { useState } from "react";
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 
-const App = () => (
-  <div
-    style={{
-      padding: 24,
-    }}
-  >
-    <SwaggerUI url="/umi-plugins_openapi.json" />
-  </div>
-);
+const App = () => {
+  const [value, setValue] = useState("openapi");
+  return (
+    <div
+      style={{
+        padding: 24,
+      }}
+    >
+      <select
+        style={{
+          position: "fixed",
+          right: "16px",
+          top: "8px",
+        }}
+        onChange={(e) => setValue(e.target.value)}
+      >
+        ${[openAPIConfig]
+          .flat(1)
+          .map((item) => {
+            return `<option value="${item.projectName || 'openapi'}">${
+              item.projectName || 'openapi'
+            }</option>`;
+          })
+          .join('\n')}
+      </select>
+      <SwaggerUI url={\`/umi-plugins_$\{value}.json\`} />
+    </div>
+  );
+};
 export default App;
 `,
     });
@@ -72,42 +104,58 @@ export default App;
     });
   }
 
+  const genOpenAPIFiles = async (openAPIConfig: any) => {
+    const openAPIJson = await getSchema(openAPIConfig.schemaPath);
+    writeFileSync(
+      join(
+        openAPIFilesPath,
+        `umi-plugins_${openAPIConfig.projectName || 'openapi'}.json`,
+      ),
+      JSON.stringify(openAPIJson, null, 2),
+    );
+  };
   api.onDevCompileDone(async () => {
     try {
       const openAPIConfig = api.config.openAPI;
-      const openAPIJson = await getSchema(openAPIConfig.schemaPath);
-      writeFileSync(
-        join(openAPIFilesPath, 'umi-plugins_openapi.json'),
-        JSON.stringify(openAPIJson, null, 2),
-      );
+      if (Array.isArray(openAPIConfig)) {
+        openAPIConfig.map((item) => genOpenAPIFiles(item));
+        return;
+      }
+      genOpenAPIFiles(openAPIConfig);
     } catch (error) {
       console.error(error);
     }
   });
+  const genAllFiles = async (openAPIConfig: any) => {
+    const pageConfig = require(join(api.cwd, 'package.json'));
+    const mockFolder = openAPIConfig.mock ? join(api.cwd, 'mock') : undefined;
+    const serversFolder = join(api.cwd, 'src', 'services');
+    // 如果mock 文件不存在，创建一下
+    if (mockFolder && !existsSync(mockFolder)) {
+      mkdirSync(mockFolder);
+    }
+    // 如果mock 文件不存在，创建一下
+    if (serversFolder && !existsSync(serversFolder)) {
+      mkdirSync(serversFolder);
+    }
 
+    await generateService({
+      projectName: pageConfig.name.split('/').pop(),
+      ...openAPIConfig,
+      serversPath: serversFolder,
+      mockFolder,
+    });
+    api.logger.info('[openAPI]: execution complete');
+  };
   api.registerCommand({
     name: 'openapi',
     fn: async () => {
       const openAPIConfig = api.config.openAPI;
-      const pageConfig = require(join(api.cwd, 'package.json'));
-      const mockFolder = openAPIConfig.mock ? join(api.cwd, 'mock') : undefined;
-      const serversFolder = join(api.cwd, 'src', 'services');
-      // 如果mock 文件不存在，创建一下
-      if (mockFolder && !existsSync(mockFolder)) {
-        mkdirSync(mockFolder);
+      if (Array.isArray(openAPIConfig)) {
+        openAPIConfig.map((item) => genAllFiles(item));
+        return;
       }
-      // 如果mock 文件不存在，创建一下
-      if (serversFolder && !existsSync(serversFolder)) {
-        mkdirSync(serversFolder);
-      }
-
-      await generateService({
-        projectName: pageConfig.name.split('/').pop(),
-        ...openAPIConfig,
-        serversPath: serversFolder,
-        mockFolder,
-      });
-      api.logger.info('[openAPI]: execution complete');
+      genAllFiles(openAPIConfig);
     },
   });
 };
