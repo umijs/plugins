fc84b45adf6a93406d0bd2ba7acd0680abe8d09e
Cyberhan123 <255542417@qq.com>
2021-03-16 10:13:13 +0800

    feat(svg-icon): 修正无法正确获取svgo配置的问题

--- packages/plugin-svg-icon/src/index.ts
+++ packages/plugin-svg-icon/src/index.ts
@@ -6,15 +6,29 @@ export default (api: IApi) => {
     config: {
       schema(Joi) {
         return Joi.object({
-          svgoConfig: Joi.any(),
+          // svgoConfig: Joi.string(),
         });
       },
     },
   });
   const srcDir = api.paths.absSrcPath;
   const svgsDir = join(srcDir!, 'assets/svgs');
-  const svgoConfigFile = api.utils.winPath(join(srcDir!, 'svgo-config.json'));
-  const svgoDefaultIcon = join(api.cwd!, '../svgo-config.json');
+  let svgoConfigDirFile = null;
+  const svgoConfigDirFilePath = api.utils.winPath(
+    join(srcDir!, '../svgo-config.json'),
+  );
+  try {
+    svgoConfigDirFile = require.resolve(svgoConfigDirFilePath);
+  } catch (e) {
+    console.log(
+      api.utils.chalk.yellow(
+        `[svg-icon]svgo-config.json doesn't find at ${svgoConfigDirFilePath}, svg-icon auto use default config`,
+      ),
+    );
+  }
+  const svgoConfigDefaultFile = require.resolve('./svgo-config.json');
+  const svgoConfigFile = svgoConfigDirFile ?? svgoConfigDefaultFile;
+  const svgoConfig = require(svgoConfigFile);
   api.chainWebpack((config) => {
     // 默认的svg的模块规则中不去匹配src/assets/svgs，避免此文件中的内容使用默认的url-loader的加载形式
     config.module.rule('svg').exclude.add(svgsDir).end();
@@ -31,9 +45,7 @@ export default (api: IApi) => {
       .end()
       .use('svgo-loader')
       .loader('svgo-loader')
-      .options(
-        api.config.svgIcon.svgoConfig || svgoConfigFile || svgoDefaultIcon,
-      );
+      .options(svgoConfig);
     return config;
   });
 };
