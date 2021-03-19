4d84817a041fd259afb6a8c7f6abfac16e7925e5
小虎oni <xiaohuoni@users.noreply.github.com>
2021-03-19 22:05:07 +0800

    feat: improve dva log (#562)
    * feat: improve dva log
    
    * fix: winPath

--- packages/plugin-dva/src/getModels/getModels.test.ts
+++ packages/plugin-dva/src/getModels/getModels.test.ts
@@ -8,8 +8,9 @@ test('getModels', () => {
   const base = join(fixtures, 'normal');
   const models = getModels({
     base,
+    cwd: __dirname,
   });
-  expect(models.map(m => relative(base, m))).toEqual([
+  expect(models.map((m) => relative(base, m))).toEqual([
     'b.js',
     'c.ts',
     'e.jsx',
@@ -21,22 +22,24 @@ test('getModels with opts.skipModelValidate', () => {
   const base = join(fixtures, 'skipModelValidate');
   const models = getModels({
     base,
+    cwd: __dirname,
     skipModelValidate: true,
   });
-  expect(models.map(m => relative(base, m))).toEqual(['no_content.js']);
+  expect(models.map((m) => relative(base, m))).toEqual(['no_content.js']);
 });
 
 test('getModels with opts.extraModels', () => {
   const base = join(fixtures, 'extraModels');
   const models = getModels({
     base,
+    cwd: __dirname,
     extraModels: [
       join(base, '..', 'models-for-extraModels', 'a_valid.js'),
       join(base, '..', 'models-for-extraModels', 'b_invalid.js'),
     ],
   });
   expect(
-    models.map(m => utils.winPath(relative(join(base, '..'), m))),
+    models.map((m) => utils.winPath(relative(join(base, '..'), m))),
   ).toEqual(['models-for-extraModels/a_valid.js']);
 });
 
@@ -44,6 +47,7 @@ test('getModels with opts.extraModels and opts.skipModelValidate', () => {
   const base = join(fixtures, 'extraModels');
   const models = getModels({
     base,
+    cwd: __dirname,
     extraModels: [
       join(base, '..', 'models-for-extraModels', 'a_valid.js'),
       join(base, '..', 'models-for-extraModels', 'b_invalid.js'),
@@ -51,9 +55,25 @@ test('getModels with opts.extraModels and opts.skipModelValidate', () => {
     skipModelValidate: true,
   });
   expect(
-    models.map(m => utils.winPath(relative(join(base, '..'), m))),
+    models.map((m) => utils.winPath(relative(join(base, '..'), m))),
   ).toEqual([
     'models-for-extraModels/a_valid.js',
     'models-for-extraModels/b_invalid.js',
   ]);
+});
+
+test('parser error when has jsx', () => {
+  const base = join(fixtures, 'jsx');
+  const filePath = join(base, 'a.jsx');
+  expect(() => {
+    getModels({
+      base,
+      cwd: __dirname,
+      skipModelValidate: false,
+    });
+  }).toThrow(
+    `Dva model ${utils.winPath(
+      relative(__dirname, filePath),
+    )} parse failed, SyntaxError: Unterminated regular expression (3:26)`,
+  );
 });
