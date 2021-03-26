08f8bbd37faeb901fbfdbbafb665152a3aeb6c7a
ycjcl868 <45808948@qq.com>
2021-03-26 12:15:40 +0800

    chore: fix targets

--- packages/plugin-esbuild/src/index.ts
+++ packages/plugin-esbuild/src/index.ts
@@ -1,5 +1,35 @@
-import { IApi, BundlerConfigType } from 'umi';
+import { IApi, BundlerConfigType, ITargets } from 'umi';
 import { ESBuildPlugin, ESBuildMinifyPlugin } from 'esbuild-loader';
+
+/**
+ * convert umi targets into esbuild targets
+ * issue: https://github.com/evanw/esbuild/issues/1060#issuecomment-807864917
+ *
+ * @param targets
+ * @returns
+ */
+export const getEsbuildTargetFromEngine = (targets: ITargets): string[] => {
+  const userTargets = targets;
+  switch (true) {
+    case 'ie' in userTargets:
+      return ['es5'];
+    default: {
+      const target = Object.keys(userTargets)
+        .map((browser) => {
+          const version = userTargets[browser];
+          if (
+            ['chrome', 'edge', 'ios', 'firefox', 'safari'].includes(browser) &&
+            Number.isSafeInteger(version)
+          ) {
+            return `${browser}${version}`;
+          }
+          return '';
+        })
+        .filter(Boolean);
+      return target?.length > 0 ? target : ['esnext'];
+    }
+  }
+};
 
 export default (api: IApi) => {
   api.describe({
@@ -14,9 +44,11 @@ export default (api: IApi) => {
 
   api.modifyBundleConfig((memo, { type }) => {
     if (memo.optimization) {
+      const targetForCSR = getEsbuildTargetFromEngine(api.config.targets || {});
+      console.log('targetForCSR', targetForCSR);
       const optsMap = {
         [BundlerConfigType.csr]: {
-          target: 'es5',
+          target: targetForCSR,
           minify: true,
         },
         [BundlerConfigType.ssr]: {
