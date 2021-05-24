cdeab65a09ef64d071da15a3db33ca12e28d6763
chenshuai2144 <qixian.cs@outlook.com>
2021-05-24 11:59:59 +0800

    fix test

--- packages/plugin-access/tests/utils/getAccessContent.test.ts
+++ packages/plugin-access/tests/utils/getAccessContent.test.ts
@@ -1,8 +1,12 @@
 import getAccessContent from '../../src/utils/getAccessContent';
 
+const utils = {
+  winPath: jest.fn(() => 'test'),
+};
+
 describe('getAccessContent', () => {
   it('should return content string when call getAccessContent', () => {
-    const result = getAccessContent();
+    const result = getAccessContent(utils);
     expect(typeof result).toBe('string');
     expect(result.length).toBeGreaterThan(0);
   });
