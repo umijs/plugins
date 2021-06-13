462883fccd2f4c2711925ad8c119feb9f2e1ad96
Kuitos <kuitos.lau@gmail.com>
2021-06-13 18:20:49 +0800

    chore(qiankun): export types (#634)

--- packages/plugin-qiankun/src/index.ts
+++ packages/plugin-qiankun/src/index.ts
@@ -3,6 +3,8 @@ import { IApi } from 'umi';
 export { isMasterEnable } from './master';
 export { isSlaveEnable } from './slave';
 
+export * from './types';
+
 export default function (api: IApi) {
   api.addRuntimePluginKey(() => 'qiankun');
 
