aec256b80e5785277ac092dff0b15ef81e98ae69
chemdemo <chemdemo@gmail.com>
2021-06-23 11:44:01 +0800

    chore(qiankun): add slave `librartName` demo

--- packages/plugin-qiankun/examples/app2/.umirc.js
+++ packages/plugin-qiankun/examples/app2/.umirc.js
@@ -1,12 +1,15 @@
 import { name } from './package.json';
 
+const libraryName = `${name.charAt(0).toUpperCase() + name.slice(1)}`;
+
 export default {
   base: name,
   publicPath: '/app2/',
   outputPath: './dist/app2',
   mountElementId: 'app2',
   qiankun: {
-    slave: {},
+    // 修改umd library name 示例
+    slave: { libraryName },
   },
   plugins: [
     require.resolve('../../../plugin-dva/lib'),
