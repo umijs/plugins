1d64e16e8255a238c2dd18cbd874217b88704575
陈帅 <qixian.cs@outlook.com>
2021-07-13 17:16:09 +0800

    prettiercode (#653)

--- packages/plugin-initial-state/src/utils/getModelContent.tsx
+++ packages/plugin-initial-state/src/utils/getModelContent.tsx
@@ -9,33 +9,41 @@ async function getInitialState() {
   return await app.getInitialState();
 }
 
-type ThenArg<T> = T extends Promise<infer U> ? U : T
+const sleep = (delay: number) => new Promise((resolve) => setTimeout(resolve, delay));
+
+type ThenArg<T> = T extends Promise<infer U> ? U : T;
 
 const initState = {
   initialState: undefined as ThenArg<ReturnType<typeof getInitialState>> | undefined,
   loading: true,
   error: undefined as Error | undefined,
-}
+};
 
 export default () => {
-  const [ state, setState ] = useState(initState);
+  const [state, setState] = useState(initState);
 
-  const refresh = useCallback(async() => {
-    setState(s => ({ ...s, loading: true, error: undefined }))
+  const refresh = useCallback(async () => {
+    setState((s) => ({ ...s, loading: true, error: undefined }));
     try {
-      const asyncFunc = () => new Promise<ReturnType<typeof getInitialState>>(res => res(getInitialState()));
+      const asyncFunc = () =>
+        new Promise<ReturnType<typeof getInitialState>>((res) => res(getInitialState()));
       const ret = await asyncFunc();
-      setState(s => ({ ...s, initialState: ret, loading: false }));
-    } catch(e) {
-      setState(s => ({ ...s, error: e, loading: false }));
+      setState((s) => ({ ...s, initialState: ret, loading: false }));
+    } catch (e) {
+      setState((s) => ({ ...s, error: e, loading: false }));
     }
+    await sleep(10);
   }, []);
 
-  const setInitialState = useCallback((initialState: ThenArg<ReturnType<typeof getInitialState>> | undefined) => {
-    setState(s => ({ ...s, initialState, loading: false }))
-  }, []);
+  const setInitialState = useCallback(
+    async (initialState: ThenArg<ReturnType<typeof getInitialState>> | undefined) => {
+     setState((s) => ({ ...s, initialState, loading: false }));
+      await sleep(10);
+    },
+    [],
+  );
 
-  useEffect(()=>{
+  useEffect(() => {
     refresh();
   }, []);
 
@@ -43,7 +51,7 @@ export default () => {
     ...state,
     refresh,
     setInitialState,
-  }
-}
+  };
+};
 `
     : 'export default () => ({ loading: false, refresh: () => {} })';
