176ae069461d51907f214efc4362b6e1209a765c
Hydrogen <fanyangliang@gmail.com>
2021-09-06 15:09:22 +0800

    fix: 删除冗余

--- packages/plugin-qiankun/src/slave/index.ts
+++ packages/plugin-qiankun/src/slave/index.ts
@@ -11,9 +11,7 @@ import { readFileSync } from 'fs';
 export function isSlaveEnable(api: IApi) {
   const slaveCfg = api.userConfig?.qiankun?.slave;
   if (slaveCfg) {
-    if (slaveCfg) {
-      return slaveCfg?.enable === false ? false : !!slaveCfg;
-    }
+    return slaveCfg?.enable === false ? false : !!slaveCfg;
   }
 
   // 兼容早期配置， qiankun 配一个空，相当于开启 slave
